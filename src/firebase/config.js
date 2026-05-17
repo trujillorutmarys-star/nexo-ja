e46434d58bf0a91eea99130c7698608f92dfe0a2
Rutmarys <trujillorutmarys@gmail.com>
2026-05-17 19:04:51 -0300

    Firebase configurado

---
+++ src/firebase/config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBI4Brim4PIaqAcbymvWMMt1X8gR4okj8c",
+  authDomain: "nexo-ja.firebaseapp.com",
+  projectId: "nexo-ja",
+  storageBucket: "nexo-ja.firebasestorage.app",
+  messagingSenderId: "3106474248",
+  appId: "1:3106474248:web:eed700308f1d2fc1bb84bd"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
